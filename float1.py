776cfb4b7510a84b904ac773afdb21c1350e0a3d
Roberto Gianfelice <rob.gianfelice@gmail.com>
2021-11-23 17:53:07 +0100

    Delete float1.py

--- float1.py
+++
@@ -1,9 +0,0 @@
-num=input("Inserisci il numero: ")
-
-posVirgola=num.find(".")
-
-parteInt=int(num[0:posVirgola])
-parteDec=float("0"+num[posVirgola:])
-
-print(f"Il numero inserito {num} è formato da {parteInt} e da {parteDec}")
-print(f"La codifica binaria di numero inserito {num} è formato {bin(parteInt)}")
